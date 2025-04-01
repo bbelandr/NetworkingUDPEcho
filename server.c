486ad514b1dc278b46529d67b08f9dfbe9f103d2
benja <bbelandr@vols.utk.edu>
2025-03-31 21:47:03 -0400

    Only sending the datagram back if the opMode is 0
    Co-authored-by: Yinyang2460 <Yinyang2460@users.noreply.github.com>
    Co-authored-by: BigFaith <BigFaith@users.noreply.github.com>

--- server.c
+++ server.c
@@ -186,18 +186,20 @@ int main(int argc, char *argv[])
       fputc('\n', stdout);
 #endif
 
-      // Send received datagram back to the client
-      ssize_t numBytesSent = sendto(sock, buffer, numBytesRcvd, 0,
-        (struct sockaddr *) &clntAddr, sizeof(clntAddr));
-      if (numBytesSent < 0) {
-        TxErrorCount++;
-        perror("server: Error on sendto ");
-        continue;
-      }
-      else if (numBytesSent != numBytesRcvd) {
-        TxErrorCount++;
-        printf("server: Error on sendto, only sent %d rather than %d ",(int32_t)numBytesSent,(int32_t)numBytesRcvd);
-        continue;
+      if (opMode == 0) {
+        // Send received datagram back to the client
+        ssize_t numBytesSent = sendto(sock, buffer, numBytesRcvd, 0,
+          (struct sockaddr *) &clntAddr, sizeof(clntAddr));
+        if (numBytesSent < 0) {
+          TxErrorCount++;
+          perror("server: Error on sendto ");
+          continue;
+        }
+        else if (numBytesSent != numBytesRcvd) {
+          TxErrorCount++;
+          printf("server: Error on sendto, only sent %d rather than %d ",(int32_t)numBytesSent,(int32_t)numBytesRcvd);
+          continue;
+        }
       }
     }
   }
