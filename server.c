11769d347dfb74d8f9d6e18de23e58c242988006
benja <bbelandr@vols.utk.edu>
2025-03-31 22:50:55 -0400

    only echo the packet at opmode = 0

--- server.c
+++ server.c
@@ -184,27 +184,27 @@ int main(int argc, char *argv[])
                msgHeaderPtr->sequenceNum,
                msgHeaderPtr->timeSentSeconds,
                msgHeaderPtr->timeSentNanoSeconds, OWDSample, smoothedOWD);
-      }
-  
-#ifdef TRACE 
-      printf("server: Rx %d bytes from ", (int32_t) numBytesRcvd);
-      fputs(" client ", stdout);
-      PrintSocketAddress((struct sockaddr *) &clntAddr, stdout);
-      fputc('\n', stdout);
-#endif
+          
+  #ifdef TRACE 
+        printf("server: Rx %d bytes from ", (int32_t) numBytesRcvd);
+        fputs(" client ", stdout);
+        PrintSocketAddress((struct sockaddr *) &clntAddr, stdout);
+        fputc('\n', stdout);
+  #endif
 
-      // Send received datagram back to the client
-      ssize_t numBytesSent = sendto(sock, buffer, numBytesRcvd, 0,
-        (struct sockaddr *) &clntAddr, sizeof(clntAddr));
-      if (numBytesSent < 0) {
-        TxErrorCount++;
-        perror("server: Error on sendto ");
-        continue;
-      }
-      else if (numBytesSent != numBytesRcvd) {
-        TxErrorCount++;
-        printf("server: Error on sendto, only sent %d rather than %d ",(int32_t)numBytesSent,(int32_t)numBytesRcvd);
-        continue;
+        // Send received datagram back to the client
+        ssize_t numBytesSent = sendto(sock, buffer, numBytesRcvd, 0,
+          (struct sockaddr *) &clntAddr, sizeof(clntAddr));
+        if (numBytesSent < 0) {
+          TxErrorCount++;
+          perror("server: Error on sendto ");
+          continue;
+        }
+        else if (numBytesSent != numBytesRcvd) {
+          TxErrorCount++;
+          printf("server: Error on sendto, only sent %d rather than %d ",(int32_t)numBytesSent,(int32_t)numBytesRcvd);
+          continue;
+        }
       }
     }
   }
